bda36159aedd9e0cb8889ab6ac64a34c08c0a00c
Patryk <patryk.sajdak@edu.uekat.pl>
2023-12-05 20:40:08 +0100

    [Fraud] Implementation of FraudCheckController

--- fraud/src/main/java/com/uekatowice/fraud/controller/FraudCheckController.java
+++ fraud/src/main/java/com/uekatowice/fraud/controller/FraudCheckController.java
@@ -1,4 +1,29 @@
 package com.uekatowice.fraud.controller;
 
+import com.uekatowice.clients.fraud.FraudCheckResponse;
+import com.uekatowice.fraud.service.FraudCheckService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/fraud-check")
 public class FraudCheckController {
+    private final FraudCheckService fraudCheckService;
+    @GetMapping(path = "{customerId}")
+    public ResponseEntity<FraudCheckResponse> isFraudster(@PathVariable("customerId") Integer customerId) {
+        log.info("Customer fraud check for customer with id: {}", customerId);
+        FraudCheckResponse fraudCheckResponse = fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("Customer with id {} fraud check response is: {}", customerId, fraudCheckResponse);
+
+        return ResponseEntity.status(CREATED).body(fraudCheckResponse);
+    }
 }
