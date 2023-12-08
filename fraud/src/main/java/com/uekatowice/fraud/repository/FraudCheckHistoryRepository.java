10982d2c860ca6951153beed6969b95a54b49de8
Patryk <patryk.sajdak@edu.uekat.pl>
2023-12-08 17:39:05 +0100

    [Fraud] Implementation of FraudCheckHistoryRepository

--- fraud/src/main/java/com/uekatowice/fraud/repository/FraudCheckHistoryRepository.java
+++ fraud/src/main/java/com/uekatowice/fraud/repository/FraudCheckHistoryRepository.java
@@ -1,4 +1,8 @@
 package com.uekatowice.fraud.repository;
 
-public class FraudCheckHistoryRepository {
+
+import com.uekatowice.fraud.entity.FraudCheckHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FraudCheckHistoryRepository extends JpaRepository<FraudCheckHistory, Integer> {
 }
