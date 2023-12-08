6674b520938ec418dd7fd6141bf0a0dc3168f3ac
dawid białek <dawid1bialek@gmail.com>
2023-12-08 21:29:37 +0100

    Start working for NotificationApplication

---
+++ src/main/java/com/dawid/notification/NotificationApplication.java
@@ -0,0 +1,16 @@
+package com.dawid.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@SpringBootApplication()
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
+public class NotificationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationApplication.class, args);
+    }
+}
