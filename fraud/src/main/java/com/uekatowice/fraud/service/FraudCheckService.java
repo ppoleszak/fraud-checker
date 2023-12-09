0b4064d421a9be2df7bc4cd3d0d1d5e047f6ccb0
Patryk <patryk.sajdak@edu.uekat.pl>
2023-12-09 15:57:26 +0100

    [Fraud] Implementation of FraudCheckService

--- fraud/src/main/java/com/uekatowice/fraud/service/FraudCheckService.java
+++ fraud/src/main/java/com/uekatowice/fraud/service/FraudCheckService.java
@@ -1,4 +1,32 @@
 package com.uekatowice.fraud.service;
 
+import com.uekatowice.clients.fraud.FraudCheckResponse;
+import com.uekatowice.fraud.entity.FraudCheckHistory;
+import com.uekatowice.fraud.repository.FraudCheckHistoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
 public class FraudCheckService {
+
+    private final FraudCheckHistoryRepository fraudCheckHistoryRepository;
+
+    public FraudCheckResponse isFraudulentCustomer(Integer customerId) {
+        boolean isCustomerFraudulent = validateIsCustomerFraudulent();
+        FraudCheckHistory fraudCheckHistory = FraudCheckHistory.builder()
+                .customerId(customerId)
+                .isFraudster(isCustomerFraudulent)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        fraudCheckHistoryRepository.save(fraudCheckHistory);
+        return new FraudCheckResponse(isCustomerFraudulent);
+    }
+
+    private boolean validateIsCustomerFraudulent() {
+        return false;
+    }
 }
